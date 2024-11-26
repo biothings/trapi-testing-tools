a2425d34e2435da344876f4ced8088d86f65add6
tokebe <43009413+tokebe@users.noreply.github.com>
2024-11-26 15:41:59 -0500

    assume ping timed out after 10s

--- trapi_testing_tools/utils.py
+++ trapi_testing_tools/utils.py
@@ -104,7 +104,7 @@ async def check_api(instance_name, instance_url, max_name_len, progress):
     task = progress.add_task(f" {instance_name:>{max_name_len}} querying...", total=1)
     try:
         response = await ASYNC_BASIC_CLIENT.get(
-            f"{instance_url}/asyncquery_status/HopefullyNonExistentHash"
+            f"{instance_url}/asyncquery_status/HopefullyNonExistentHash", timeout=10
         )
         if not response.status_code == 404:
             response.raise_for_status()
