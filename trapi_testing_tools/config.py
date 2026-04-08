1bbc752d434625a04db544e060b8221470514a5b
tokebe <43009413+tokebe@users.noreply.github.com>
2026-04-08 16:54:52 -0700

    better defaults handling

--- trapi_testing_tools/config.py
+++ trapi_testing_tools/config.py
@@ -1,6 +1,33 @@
-from typing import ClassVar, override
+from copy import deepcopy
+from typing import ClassVar, Self, override
 
-from pydantic_settings import BaseSettings, PydanticBaseSettingsSource, SettingsConfigDict, YamlConfigSettingsSource
+from pydantic import Field, field_validator
+from pydantic_settings import (
+    BaseSettings,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+    YamlConfigSettingsSource,
+)
+
+DEFAULT_ENVS = {
+    "ars": {
+        "prod": "https://ars-prod.transltr.io/ars/api/messages",
+        "test": "https://ars.test.transltr.io/ars/api/messages",
+        "ci": "https://ars.ci.transltr.io/ars/api/messages",
+        "dev": "https://ars-dev.transltr.io/ars/api/messages",
+    },
+    "retriever": {
+        "local": "http://localhost:8080",
+        "ci": "https://retriever.ci.transltr.io",
+        "dev": "https://dev.retriever.biothings.io",
+    },
+    "shepherd": {
+        "aragorn.dev": "https://shepherd.renci.org/aragorn",
+        "arax.dev": "https://shepherd.renci.org/arax",
+        "bte.dev": "https://shepherd.renci.org/bte",
+        "sipr.dev": "https://shepherd.renci.org/sipr",
+    },
+}
 
 
 class TTTConfig(BaseSettings):
@@ -9,13 +36,19 @@ class TTTConfig(BaseSettings):
     timeout: float = 300
     test_repo: str = "NCATSTranslator/Tests"
     default_environment: str = "retriever"
-    environments: dict[str, dict[str, str]] = {
-        "retriever": {
-            "local": "http://localhost:8080",
-            "ci": "https://retriever.ci.transltr.io",
-            "dev": "https://dev.retriever.biothings.io",
-        },
-    }
+    environments: dict[str, dict[str, str]] = Field(
+        default_factory=lambda: DEFAULT_ENVS
+    )
+
+    @field_validator("environments", mode="after")
+    @classmethod
+    def include_default(
+        cls, value: dict[str, dict[str, str]]
+    ) -> dict[str, dict[str, str]]:
+        """Ensure the defaults are included, with the config taking precedence."""
+        envs = deepcopy(DEFAULT_ENVS)
+        envs.update(value)
+        return envs
 
     model_config: ClassVar[SettingsConfigDict] = SettingsConfigDict(
         case_sensitive=False,
@@ -25,7 +58,6 @@ class TTTConfig(BaseSettings):
         yaml_file="config.yaml",
         yaml_file_encoding="utf-8",
     )
-
 
     @classmethod
     @override
@@ -44,5 +76,6 @@ class TTTConfig(BaseSettings):
             YamlConfigSettingsSource(settings_cls),
             init_settings,
         )
+
 
 CONFIG = TTTConfig()
