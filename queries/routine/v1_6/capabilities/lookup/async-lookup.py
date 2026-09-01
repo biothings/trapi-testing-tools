9b3c6941742d8565c5d2bebc8a96ee4fcc040a4c
tokebe <43009413+tokebe@users.noreply.github.com>
2026-09-01 15:54:44 -0400

    update async-lookup

--- queries/routine/v1_6/capabilities/lookup/async-lookup.py
+++ queries/routine/v1_6/capabilities/lookup/async-lookup.py
@@ -8,13 +8,13 @@ body = {
         "query_graph": {
             "nodes": {
                 "n0": {"categories": ["biolink:Gene"], "ids": ["NCBIGene:3778"]},
-                "n1": {"categories": ["biolink:Gene"]},
+                "n1": {"categories": ["biolink:Disease"]},
             },
             "edges": {
                 "e01": {
                     "subject": "n0",
                     "object": "n1",
-                    "predicates": ["biolink:regulates"],
+                    "predicates": ["biolink:related_to"],
                 }
             },
         }
